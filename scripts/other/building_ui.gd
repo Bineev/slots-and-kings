d608d6cc640c40e49bcb168f7f4a4af9a92a747c
tafari <sbineev@gmail.com>
2025-11-28 22:25:05 +0300

    Add shop layout

--- scripts/other/building_ui.gd
+++ scripts/other/building_ui.gd
@@ -8,11 +8,21 @@ class_name BuildingUI
 @onready var buildings_container: GridContainer = %buildings_container
 
 var building_previews : Array[BuildingPreview]
-
+var is_should_check_input : bool = true
 
 func _ready() -> void:
 	initialize()
 
+
+func _input(event):
+	if not is_should_check_input:
+		return
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == MOUSE_BUTTON_RIGHT:
+			visible = false
+			is_should_check_input = false
+			var tween = get_tree().create_tween()
+			tween.tween_callback(destroy).set_delay(0.5)
 
 func initialize():
 	await self.ready
@@ -20,3 +30,7 @@ func initialize():
 		var building = building_scene.instantiate()
 		buildings_container.add_child(building)
 		building.initialize()
+
+
+func destroy():
+	queue_free()
