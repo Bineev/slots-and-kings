ec624f0a69b3e0625645cae99c88cb9a33804ee5
tafari <sbineev@gmail.com>
2025-12-16 12:50:44 +0300

    correct some bugs

--- resources/passive_skill_res.gd
+++ resources/passive_skill_res.gd
@@ -11,4 +11,7 @@ class_name PassiveSkillRes
 @export var skill_target_type : DataManager.UnitOwner
 @export var skill_damage_type : DataManager.UnitType
 @export var skill_buff_stats : Array[Dictionary]
+@export var skill_resources : Array[DataManager.ResType]
+@export var skill_resources_amounts : Array[int]
+@export var skill_health_amount : int
 # {stat_name : current_stat, stat_change_type : 0 (+) or 1 (*), stat_change_amount : float}
--- scripts/other/passive_skill.gd
+++ scripts/other/passive_skill.gd
@@ -7,7 +7,9 @@ class_name PassiveSkill
 @export var skill_passive_type : DataManager.PassiveSkillType
 @export var skill_damage_type : DataManager.UnitType
 @export var skill_buff_stats : Array[Dictionary]
-
+@export var skill_resources : Array[DataManager.ResType]
+@export var skill_resources_amounts : Array[int]
+@export var skill_health_amount : int
 
 func initialize():
 	await get_tree().process_frame
@@ -18,7 +20,9 @@ func initialize():
 	skill_desc = skill_res.skill_desc
 	skill_damage_type = skill_res.skill_damage_type
 	skill_target_type = skill_res.skill_target_type
-	# инициализируем статы
+	skill_resources = skill_res.skill_resources
+	skill_resources_amounts = skill_res.skill_resources_amounts
+	skill_health_amount = skill_res.skill_health_amount
 	# пересчитываем исходя из стат героя
 	recalculate_stats()
 	texture = skill_res.skill_preview
