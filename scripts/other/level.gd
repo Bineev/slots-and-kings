dd9f91c6235d56c456728d7e043aaeb322182410
tafari <sbineev@gmail.com>
2025-12-20 12:42:08 +0300

    correct some bugs

--- scripts/other/level.gd
+++ scripts/other/level.gd
@@ -429,7 +429,7 @@ func sort_player_units():
 	var units : Array[Unit] = Player.get_player_units()
 	if units.size() == 0:
 		return
-	units.sort_custom(func(a, b): return a.global_position.y < b.global_position.y)
+	units.sort_custom(func(a, b): return a.global_position.y + a.unit_sprite.texture.get_height() / 5 / 2 < b.global_position.y + b.unit_sprite.texture.get_height() / 5 / 2)
 	var base_ordering : int = 43
 	for unit in units:
 		base_ordering += 1
